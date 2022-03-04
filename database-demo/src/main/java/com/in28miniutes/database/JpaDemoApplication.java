52c03fb16c4e5368f493e26c937e86b7571b1648
praveen-1408 <100114320+praveen-1408@users.noreply.github.com>
2022-03-04 04:19:43 +0000

    Add files via upload

---
+++ database-demo/src/main/java/com/in28miniutes/database/JpaDemoApplication.java
@@ -0,0 +1,47 @@
+package com.in28miniutes.database;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.in28miniutes.database.entity.Person;
+import com.in28miniutes.database.jpa.PersonJpaRepository;
+
+@SpringBootApplication
+public class JpaDemoApplication implements CommandLineRunner {
+
+private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+@Autowired
+PersonJpaRepository repository;
+
+public static void main(String[] args) {
+SpringApplication.run(JpaDemoApplication.class, args);
+}
+
+@Override
+public void run(String... args) throws Exception {
+
+
+logger.info("Inserting -> {}",
+repository.insert(new Person("Tara", "Berlin", new Date())));
+
+logger.info("Inserting -> {}",
+repository.insert(new Person( "Pieter", "Utrecht", new Date())));
+
+Person person = repository.findById(1);
+logger.info("Employee -> {}",person);
+
+Person updatedPerson = repository.updatePerson(2,"Pieter", "london", new Date());
+logger.info("Updated Employee -> {}",updatedPerson);
+
+
+
+logger.info("All users -> {}", repository.findAll());
+}
+} 
---
+++ database-demo/src/main/java/com/in28miniutes/database/jpa/PersonJpaRepository.java
@@ -0,0 +1,55 @@
+package com.in28miniutes.database.jpa;
+
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.in28miniutes.database.entity.Person;
+@Repository
+@Transactional
+public class PersonJpaRepository {
+	
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	public List<Person> findAll() {
+		TypedQuery<Person> namedQuery = entityManager.createNamedQuery("find_all_persons", Person.class);
+		return namedQuery.getResultList();
+	}
+	
+	public Person findById(int id) {
+		return entityManager.find(Person.class, id);// JPA
+	}
+
+	
+
+	public Person insert(Person person) {
+		return entityManager.merge(person);
+	}
+
+	
+
+	public Person updatePerson(int id, String name, String location,Date birthDate) {
+		Person person = findById(id);
+		person.setName(name);
+		person.setLocation(location);
+		person.setBirthdate(birthDate);
+		entityManager.merge(person);
+		
+		return person;
+		
+	}
+
+
+
+	
+
+
+}
